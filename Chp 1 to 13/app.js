5f4ceb9fd21a74a29ac68046fdad311ae2318a58
shahbaazkyz@gmail.com <nabiasheikh2222@gmail.com>
2021-07-05 12:31:03 +0500

    Alerts

---
+++ Chp 1 to 13/app.js
@@ -0,0 +1,11 @@
+function q1() {
+  alert("Welcome to this Series!");
+}
+
+function q2() {
+  alert("Please Enter Valid Password!");
+}
+
+function q3() {
+  alert("Welcome to JS Land \n Happy Coding!!");
+}
